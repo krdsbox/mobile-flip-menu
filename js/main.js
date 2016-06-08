7ba708d3a7356d380e42e8fe0f4203f3a5f5df58
Logesh Kumar <logesh.kumar@krds.fr>
2016-06-08 13:15:40 +0530

    initial mobile flip menu

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
-// your js here
-window.console && console.log("Welcome to box starter");
+$(document).ready(function(){
+	// menu click event
+	$('.menuBtn').click(function() {
+		$(this).toggleClass('act');
+		$('.mainMenu, .mainContainer').toggleClass('act');
+	});
+
+	$('.mainMenu li').click(function() {
+		$('.mainMenu li').removeClass('act');
+		$(this).addClass('act');
+		$('.menuBtn').removeClass('act');
+		$('.mainMenu, .mainContainer').removeClass('act');
+	});
+});
